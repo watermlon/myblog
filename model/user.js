9e766c4708fc6fa5b4fc78532ddc708de5123d1d
zhc <zhcmoe@163.com>
2018-04-12 16:21:50 +0800

    用户信息

--- model/user.js
+++ model/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 let userSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    introduction: String,
-    avatar: String,
-    createtime: String
+    username: String,//用户名
+    password: String,//密码
+    introduction: String,//简介
+    avatar: String,//头像
+    createtime: String//创建时间
 })
 let userModel = mongoose.model('user', userSchema,'user')
 
--- router/user.js
+++ router/user.js
@@ -1,9 +1,21 @@
 const userModal = require('../model').user
 let get = function(req, res, next){
+    //查询所有数据
     userModal.find({},function (err, fluffy) {
         if (err) {
             console.log(err)
         } else {
+            let sendArr = []
+            fluffy.forEach(v=>{
+                sendArr.push({
+                    id:v.id,
+                    avatar:v.avatar,
+                    createtime:v.createtime,
+                    introduction:v.introduction,
+                    username:v.username
+                })
+            })
+            res.send(sendArr)
             console.log('success',fluffy)
         }
     })
