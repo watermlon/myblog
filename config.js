136a90b3819bffb065e59aeb67ccd6e0f9da2bb6
zhc <zhcmoe@163.com>
2018-06-29 10:35:53 +0800

    build

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports  = {
     // tokenSear : 'H974X5dG3m',
     dbUsername : 'root',
-    dbpassword:'@KANCOLLE_',
+    dbpassword:'',
     dbName:'blog',
     dbUrl:'101.200.63.181:27017'
 }
